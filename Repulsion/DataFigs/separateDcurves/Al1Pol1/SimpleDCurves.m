7f4f52e02f4d464bd7477deec033d5bc339fa415
Alexandr Tatarkin <tiamatory@gmail.com>
2024-11-19 16:06:44 +0300

    RPolesTracer to release

--- Repulsion/DataFigs/separateDcurves/Al1Pol1/SimpleDCurves.m
+++ Repulsion/DataFigs/separateDcurves/Al1Pol1/SimpleDCurves.m
@@ -1,4 +1,8 @@
 clc; close all; clear;
 
-simpleDC = load("Dcurves\simpleDcurves.txt");
-plot(simpleDC(:,1), simpleDC(:,2), '.')
+RPDots = load("Dcurves/simpleDcurves.txt");
+RPole1 = load("Dcurves/1.txt");
+RPole2 = load("Dcurves/2.txt");
+RPole3 = load("Dcurves/3.txt");
+plot(RPDots(:,1), RPDots(:,2), '.', RPole1(:,1), RPole1(:,2), RPole2(:,1), RPole2(:,2), RPole3(:,1), RPole3(:,2))
+
--- Repulsion/separateDcurves.f90
+++ Repulsion/separateDcurves.f90
@@ -1,21 +1,21 @@
     MODULE SDC_globals
-    integer resFileNo, freqsNum, SDcurvesNum, dotsNum
+    integer resFileNo, freqsNum, SDcurvesNum
     character (len=40) curvesDir
     real*8 fOld, alfaOld, SDCstep, SDCeps, fMax
     real*8 fMin, fStep, dzMin, dzMax, haminStep, haminEps
     complex*16 pOld, p_c
     
-    real*8, allocatable:: freqs(:), dzetas(:), resK11(:), f_sp(:), alfa_sp(:)
+    real*8, allocatable:: freqs(:), dzetas(:), resK11(:), p1(:,:), p2(:,:)
     namelist /basicInfo/SDcurvesNum, fmax, SDCstep, SDCeps, curvesDir
-    namelist /startPoints/ f_sp, alfa_sp, fMax
-    namelist /simpleDC/ dotsNum, fMin, dzMin, dzMax, haminStep, haminEps
+    namelist /startPoints/ p1, p2
+    namelist /simpleDC/ freqsNum, fMin, dzMin, dzMax, haminStep, haminEps
     
     CONTAINS 
         subroutine initSeparateDcurves
         IMPLICIT NONE
             open(unit=1,file='SeparateDcurves settings.txt',status='old')
             read(1, basicInfo)
-            allocate(f_sp(SDcurvesNum), alfa_sp(SDcurvesNum))
+            allocate(p1(3,SDcurvesNum), p2(3,SDcurvesNum))
             read(1, startPoints)
             read(1, simpleDC)
             close(1)       
@@ -24,7 +24,7 @@
     
     
     
-    SUBROUTINE simpleDcurves
+    SUBROUTINE RPolesDots
     use Mult_Glob;            ! GIVES US w, f, pi
     use SDC_globals;
     IMPLICIT NONE
@@ -36,9 +36,9 @@
         dz = 0d0;
         vs = sqrt(C_6(6,6,1)/rho(1))
         open(unit=301, file='.\DataFigs\separateDcurves\'//trim(curvesDir)//'\Dcurves\simpleDcurves.txt', status='unknown')
-        fstep = (fmax - fmin)/dotsNum;
+        fstep = (fmax - fmin)/freqsNum;
         print*, 'Regular, non separate d curver plot has been started'
-        do i = 1, dotsNum
+        do i = 1, freqsNum
             f = fmin + fstep*(i-1); w = f*2d0*pi; kap2=w*hs(1)/vs;
             call Hamin(RDabs, dzMin, dzMax, haminStep, haminEps, 20, dz, Ndz)
             do j = 1, Ndz
@@ -46,7 +46,7 @@
             enddo    
         enddo
         close(301)
-    END SUBROUTINE simpleDcurves
+    END SUBROUTINE RPolesDots
     
     
                                                                 ! auxiliary function for real poles finding by Hamin
@@ -64,7 +64,7 @@
     end    
     
     
-    function arcDelta(angle) result(delta)
+    function arcHaminDelta(angle) result(delta)
     use Mult_Glob; 
     use SDC_globals;
     implicit none
@@ -80,103 +80,71 @@
     
     
 
-    subroutine SCP1(f0, alfa0, file)  ! Находит точки дисперсионной кривой и записывает в файл соотв значения частоты и полюса, не переписывалась еще с момента как заработала
-    use SDC_globals;
-    use Mult_Glob; 
-    implicit none
-    integer  Ndz, choice, iterno, j, file
-    real*8 f0, alfa0, fNew, alfaNew, step, dz(20), psi, arcRDabs
-    external arcRDabs
-    !    !                                                                    первые шаги, подготовка к автоматике
-        step = SDCstep; fOld = f0; alfaOld = alfa0;
-        call Hamin(arcRDabs, 0d0, pi, 2d-3, SDCeps, 20, dz, Ndz)
-        alfaNew = sin(dz(1))*SDCstep + alfaOld; fNew = cos(dz(1))*SDCstep + fOld;
-        psi = atan( (fNew-fOld)/(alfaNew-alfaOld) );      
-        w = 2d0*pi*fNew       
-        write(file, '(4E15.6E3)') fNew, alfaNew
-        fNew = fOld; alfaNew = alfaOld;
-    !                                                                         автоматический режим
-        do 
-            iterno= 0;
-            do
-                call Hamin(arcRDabs, pi/3d0-psi, 2d0*pi/3d0-psi, 0.5d-3, SDCeps, 4, dz, Ndz)
-                if (Ndz>1) then
-                    !print*, Ndz
-                    choice = 1; 
-                    do j = 2, Ndz
-                        if ( abs(dz(j)-(pi/2d0 - psi)) < abs(dz(j-1)-(pi/2d0 - psi)) ) choice = j
-                    enddo
-                    exit;
-                else
-                    if (step < SDCstep) SDCstep = SDCstep*2d0
-                     choice = 1; exit;
-                endif    
-                iterno = iterno + 1; if (iterno > 5) exit;
-            enddo
-            alfaNew = sin(dz(choice))*SDCstep + alfaOld; fNew = cos(dz(choice))*SDCstep + fOld;
-            w = 2d0*pi*fNew        
-            write(file, '(4E15.6E3)') fNew, alfaNew
-            psi = atan( (fNew-fOld)/(alfaNew-alfaOld) ); fOld = fNew; alfaOld = alfaNew;
-            if (fNew>fmax) exit;
-        enddo 
-    end subroutine SCP1
+
     
     
-    subroutine RPolesTracer(p1, p2, fileNo, arcStep, radStep, logNo)
-    use SDC_globals
+    subroutine RPoleTracer(p1, p2, fileNo, arcStep, radStep, fMax, logNo)
+    use SDC_globals, only: pOld, p_c, SDCeps
     use Mult_Glob
     implicit none
+    logical halfed
     integer fileNo, logNo, Ndz, forward
-    real*8 arcStep, radStep, dz(20), arcDelta
+    real*8 arcStep, radStep, fMax, dz(20), arcHaminDelta, oldRadStep
     real*8, allocatable:: abs_dz(:)
     complex*16 p, p1, p2
-    external arcDelta
-        write(fileNo, '(5E15.6E3)') p1
-        write(fileNo, '(5E15.6E3)') p2
+    external arcHaminDelta
+        write(fileNo, 2) p1
+        write(fileNo, 2) p2
+        oldRadStep = RadStep
         do 
             p_c = p2 - p1; p_c = p_c/abs(p_c)*radStep;
             pOld = p2;
-            call Hamin(arcDelta, -0.75d0*pi, 0.75d0*pi, arcStep, SDCeps, 20, dz, Ndz)
+            if (real(p2) > fmax .OR. real(p2)<=0d0) exit;
+            call Hamin(arcHaminDelta, -0.75d0*pi, 0.75d0*pi, arcStep, SDCeps, 20, dz, Ndz)
             if (Ndz == 1) then 
                 p1 = p2; p2 = p2 + p_c*exp(cci*dz(1));
-                write(fileNo, '(5E15.6E3)') real(p2), imag(p2)
+                write(fileNo, 2) real(p2), imag(p2)
+                radStep = oldRadStep;
             else if (Ndz>1) then
-                print*, 'RPolesTracer: To many poles!', Ndz
+                !print*, 'RPolesTracer: To many poles!', Ndz
                 allocate(abs_dz(Ndz))
                 abs_dz = abs(dz(1:Ndz))
                 forward = minloc(abs_dz, DIM = 1)
                 deallocate(abs_dz)
                 p1 = p2; p2 = p2 + p_c*exp(cci*dz(forward));
-                write(fileNo, '(5E15.6E3)') real(p2), imag(p2)
+                write(fileNo, 2) real(p2), imag(p2)
+                radStep = oldRadStep;
             else 
-                print*, 'RPolesTracer: No poles at all!', Ndz
+                !print*, 'RPolesTracer: No poles at all!', Ndz
+                radStep = radStep/1.5d0; halfed = .TRUE.;
+                !print*, 'RPolesTracer: Half step!', radStep
             endif
         enddo
-    
+ 2       format(3F30.16)   
     end
     
     
-    subroutine plotAllcurves ! открывает папку с файлами дисп. кривых, строит кривые от начальных точек f_sp(i), alfa_sp(i)
+    subroutine RPoleCurves ! открывает папку с файлами дисп. кривых, строит кривые от начальных точек f_sp(i), alfa_sp(i)
     use SDC_globals;
     implicit none
     integer i, fileNum
-    complex*16 p1, p2
+    complex*16 theP1, theP2
     character(len=20) fileName, str
     external str
         call initSeparateDcurves
         print*, 'Separate disp curves plotting has been started!'
-        do i = 1, SDcurvesNum
+        do i = 3, SDcurvesNum
             fileName = str(i)
             fileNum = i+300
+            theP1 = p1(1, i) + (0d0, 1d0)*p1(2, i); 
+            theP2 = p2(1, i) + (0d0, 1d0)*p2(2, i); 
             print*, 'Curve ', i, ' plotting has been started!'
             open(unit=fileNum, file='.\DataFigs\separateDcurves\'//trim(curvesDir)//'\Dcurves\'//trim(fileName)//'.txt', FORM='FORMATTED')
-            !call SCP1(f_sp(i), alfa_sp(i), fileNum)
-            p1 = cmplx(0.116311d-1, 0.213963d0); p2 = cmplx(0.127131d-1,0.223904 );
-            call RPolesTracer(p1, p2, fileNum, 1d-2, SDCstep, 2)
+            call RPoleTracer(theP1, theP2, fileNum, 1d-2, SDCstep, fmax, 2)
             close(fileNum)
             print*, 'Curve ', i, 'is done!'
         enddo    
-    end subroutine plotAllcurves
+    end subroutine RPoleCurves
     
     
     
@@ -184,7 +152,7 @@
     
     
     
-    subroutine residuesFromDCurves ! открывает папку с файлами дисп. кривых, строит соотв. файлы кривых амплитуд
+    subroutine resFromRPoles ! открывает папку с файлами дисп. кривых, строит соотв. файлы кривых амплитуд
     use SDC_globals;
     implicit none
     integer i, j, oldFileNum, newFileNum, io
@@ -258,3 +226,66 @@
         write (str, *) k
         str = adjustl(str)
     end function str
+    
+    
+    subroutine SCP1(f0, alfa0, file)  ! Находит точки дисперсионной кривой и записывает в файл соотв значения частоты и полюса, не переписывалась еще с момента как заработала
+    use SDC_globals;
+    use Mult_Glob; 
+    implicit none
+    integer  Ndz, choice, iterno, j, file
+    real*8 f0, alfa0, fNew, alfaNew, step, dz(20), psi, arcRDabs
+    external arcRDabs
+    !    !                                                                    первые шаги, подготовка к автоматике
+        step = SDCstep; fOld = f0; alfaOld = alfa0;
+        call Hamin(arcRDabs, 0d0, pi, 2d-3, SDCeps, 20, dz, Ndz)
+        alfaNew = sin(dz(1))*SDCstep + alfaOld; fNew = cos(dz(1))*SDCstep + fOld;
+        psi = atan( (fNew-fOld)/(alfaNew-alfaOld) );      
+        w = 2d0*pi*fNew       
+        write(file, '(4E15.6E3)') fNew, alfaNew
+        fNew = fOld; alfaNew = alfaOld;
+    !                                                                         автоматический режим
+        do 
+            iterno= 0;
+            do
+                call Hamin(arcRDabs, pi/3d0-psi, 2d0*pi/3d0-psi, 0.5d-3, SDCeps, 4, dz, Ndz)
+                if (Ndz>1) then
+                    !print*, Ndz
+                    choice = 1; 
+                    do j = 2, Ndz
+                        if ( abs(dz(j)-(pi/2d0 - psi)) < abs(dz(j-1)-(pi/2d0 - psi)) ) choice = j
+                    enddo
+                    exit;
+                else
+                    if (step < SDCstep) SDCstep = SDCstep*2d0
+                     choice = 1; exit;
+                endif    
+                iterno = iterno + 1; if (iterno > 5) exit;
+            enddo
+            alfaNew = sin(dz(choice))*SDCstep + alfaOld; fNew = cos(dz(choice))*SDCstep + fOld;
+            w = 2d0*pi*fNew        
+            write(file, '(4E15.6E3)') fNew, alfaNew
+            psi = atan( (fNew-fOld)/(alfaNew-alfaOld) ); fOld = fNew; alfaOld = alfaNew;
+            if (fNew>fmax) exit;
+        enddo 
+    end subroutine SCP1
+    
+    
+    !subroutine plotAllcurves ! открывает папку с файлами дисп. кривых, строит кривые от начальных точек f_sp(i), alfa_sp(i)
+    !use SDC_globals;
+    !implicit none
+    !integer i, fileNum
+    !complex*16 p1, p2
+    !character(len=20) fileName, str
+    !external str
+    !    call initSeparateDcurves
+    !    print*, 'Separate disp curves plotting has been started!'
+    !    do i = 1, SDcurvesNum
+    !        fileName = str(i)
+    !        fileNum = i+300
+    !        print*, 'Curve ', i, ' plotting has been started!'
+    !        open(unit=fileNum, file='.\DataFigs\separateDcurves\'//trim(curvesDir)//'\Dcurves\'//trim(fileName)//'.txt', FORM='FORMATTED')
+    !        call SCP1(f_sp(i), alfa_sp(i), fileNum)
+    !        close(fileNum)
+    !        print*, 'Curve ', i, 'is done!'
+    !    enddo    
+    !end subroutine plotAllcurves
